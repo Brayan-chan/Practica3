53517eb0adbeef94a5c25f2496aa80bc1f1b755f
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-09-21 14:28:24 -0600

    PracticaAlumnos

---
+++ AlumnoPractica/Alumno.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Alumno {
+    
+    public static void alumno() {
+        String nombre = "Brayan Nathanael Chan Pacheco";
+        String carrera = "ISC";
+        int matricula = 71392;
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
